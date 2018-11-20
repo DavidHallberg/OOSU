6239d5fb886ad73b7a1219ef74f6c06c5a181cb2
DavidHallberg <davidhallberg@live.se>
2018-11-20 17:36:47 +0100

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Person
+    {
+        public string Fnamn;
+        public string Lnamn;
+        public int Age;
+        public int Gender;
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] pers = new Person[10];
+
+       
+            for(int i = 0; i < 5; i++){
+                pers[i] = new Person();
+                Console.WriteLine("Enter firstname: ");
+                pers[i].Fnamn = Console.ReadLine();
+                Console.WriteLine("Enter lastname: ");
+                pers[i].Lnamn = Console.ReadLine();
+                Console.WriteLine("Enter age: ");
+                pers[i].Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter gender: ");
+                pers[i].Gender = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("_______________________");
+                /*
+                Console.WriteLine(pers[i].Fnamn);
+                Console.WriteLine(pers[i].Lnamn);
+                Console.WriteLine(pers[i].Age);
+                Console.WriteLine(pers[i].Gender);
+                */
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(pers[i].Fnamn);
+                Console.WriteLine(pers[i].Lnamn);
+                Console.WriteLine(pers[i].Age);
+                Console.WriteLine(pers[i].Gender);
+            }
+            
+
+
+
+                /*
+                int x = 0;
+                int y = 0;
+                double z = Math.PI;
+
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(x);
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(y);
+                Console.WriteLine(z);
+
+                */
+                Console.ReadLine();
+        }
+
+    }
+}
